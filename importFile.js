ae5201ef695e5c2f532aa4c2cf58ec73a3a1990f
agonglach <46351725+agonglach@users.noreply.github.com>
2019-09-22 21:54:56 -0400

    Update importFile.js

--- importFile.js
+++ importFile.js
@@ -1,6 +1,6 @@
 var reader = new FileReader();
 reader.onload = function (e) {
-    var textArea = document.getElementById("myTextArea");
+    var textArea = document.getElementById("textArea1");
     textArea.value = e.target.result;
 };
 reader.readAsText(file);
