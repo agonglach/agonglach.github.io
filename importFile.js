20e33c651ced2da2b61165c92cf72ccc24ab4743
agonglach <46351725+agonglach@users.noreply.github.com>
2019-09-23 16:59:24 -0400

    Update importFile.js

--- importFile.js
+++ importFile.js
@@ -1,13 +1,13 @@
 
-function fileToTextBox() {
+function fileToTextBox(var myFile) {
   
-  var file = document.getElementById("myFile").files[0];
+      //var file = document.getElementById("myFile").files[0];
 
-  var reader = new FileReader();
-  reader.onload = function (e) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
       var textArea = document.getElementById("text1");
       textArea.value = e.target.result;
-  };
-  reader.readAsText(file);
+  
+      reader.readAsText(myFile);
   
 }  
