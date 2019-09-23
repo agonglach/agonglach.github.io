a1c5c627886ecde80860fc0a9a67b21749f3a05f
agonglach <46351725+agonglach@users.noreply.github.com>
2019-09-22 21:12:35 -0400

    Update and rename main.js to importFile.js

---
+++ importFile.js
@@ -0,0 +1,6 @@
+var reader = new FileReader();
+reader.onload = function (e) {
+    var textArea = document.getElementById("myTextArea");
+    textArea.value = e.target.result;
+};
+reader.readAsText(file);
